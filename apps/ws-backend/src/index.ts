bf4bccd3ec8d4f27caa110ce9fdcb17b1da1ee30
Vishesh Dwivedi <visheshdwivedi225544@gmail.com>
2025-06-02 17:37:47 +0530

    feat:Intialized the Backends and the Project

---
+++ apps/ws-backend/src/index.ts
@@ -0,0 +1,10 @@
+import {WebSocketServer} from "ws";
+
+const wss= new WebSocketServer({ port: 8080 });
+wss.on("connection", function connection(ws:any) {
+     console.log("New Client COnnected");
+     ws.on("message", function message(data: any) {
+          console.log("received: %s", data);
+          ws.send(`Hello, you sent -> ${data}`);
+     });
+});
