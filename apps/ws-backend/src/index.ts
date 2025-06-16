a708174ef3d2392eacb903f5d40a7d28bffb326a
Vishesh Dwivedi <visheshdwivedi225544@gmail.com>
2025-06-17 04:09:34 +0530

    feat: ws auth

--- apps/ws-backend/src/index.ts
+++ apps/ws-backend/src/index.ts
@@ -1,9 +1,14 @@
 import {WebSocketServer} from "ws";
 
 const wss= new WebSocketServer({ port: 8080 });
-wss.on("connection", function connection(ws:any) {
+wss.on("connection", function connection(ws:any, req:any) {
      console.log("New Client Connected");
      ws.on("message", function message(data: any) {
+          //auth check 
+          //fetch token from url
+          const url= req.url;
+
+
           console.log("received: %s", data);
           ws.send(`Hello, you sent -> ${data}`);
      });
