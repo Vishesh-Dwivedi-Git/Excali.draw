c65d6c3afb52d283bf06d60da3ca28f89cfe8c1e
Vishesh Dwivedi <visheshdwivedi225544@gmail.com>
2025-06-17 17:48:28 +0530

    feat: 22.1 complete till basic auth and server

--- apps/ws-backend/src/index.ts
+++ apps/ws-backend/src/index.ts
@@ -1,4 +1,5 @@
 import {WebSocketServer} from "ws";
+import jwt from "jsonwebtoken";
 
 const wss= new WebSocketServer({ port: 8080 });
 wss.on("connection", function connection(ws:any, req:any) {
@@ -7,7 +8,16 @@ wss.on("connection", function connection(ws:any, req:any) {
           //auth check 
           //fetch token from url
           const url= req.url;
-
+          const param=new URLSearchParams(url.split('?')[1]);
+          const token= param.get("token") as string;
+          const decodedToken=jwt.verify(token,process.env.JWT_SECRET as string);
+          if(!decodedToken){
+                ws.close(1008, "Authentication failed");
+                return;
+          }
+          //if token is valid, proceed with the message
+          // Here you can also check the user ID or any other information from the token
+          const id= (decodedToken as any).id;
 
           console.log("received: %s", data);
           ws.send(`Hello, you sent -> ${data}`);
